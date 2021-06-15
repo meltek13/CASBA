29e259f88ace6278a5fb426f8197d88b9e3b4f77
meltek13 <regnault.melvin@hotmail.fr>
2021-06-15 13:27:28 +0200

    add: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :calendars
-  resources :flatsharings
   devise_for :users,
              controllers: {
                  sessions: 'users/sessions',
                  registrations: 'users/registrations'
              }
   resources :members
+  resources :calendars
+  resources :flatsharings
 end
