1063486548c722d8aa940167e33f94d668ba1ae2
frederic <fredericseguier@yahoo.fr>
2021-06-15 17:03:15 +0200

    ADD: letter_opener, development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar
@@ -7,4 +8,9 @@ class User < ApplicationRecord
   :jwt_authenticatable,
   :registerable,
   jwt_revocation_strategy: JwtDenylist
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
