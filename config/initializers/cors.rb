daff7b4da1397b024913991a581537552e0e3155
meltek13 <regnault.melvin@hotmail.fr>
2021-06-21 17:56:40 +0200

    update 1000eme test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
         headers: :any,
-        methods: :any,
+        methods: [:get, :post, :patch, :put, :delete, :options, :head],
         expose: ['Authorization']
   end
 end
