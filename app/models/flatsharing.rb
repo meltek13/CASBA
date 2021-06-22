e291d33e1b65747ad491309a513b51835c981da9
Kelvi3 <theocazenavecoupet@gmail.com>
2021-06-23 01:44:42 +0200

    ADD: validate for title flatsharing

--- app/models/flatsharing.rb
+++ app/models/flatsharing.rb
@@ -1,11 +1,12 @@
 class Flatsharing < ApplicationRecord
    #after_create :invitation_to_join_flat
+   validates :title, presence: true
    has_one :token
    has_many :users
    has_many :expenses
    serialize :pending_invitation, Array
    serialize :flat_mate, Array
-
+   
    def invitation_to_join_flat
      InvitationMailer.invitation_email(self).deliver_now
    end
