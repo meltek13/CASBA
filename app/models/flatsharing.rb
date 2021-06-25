abe95e12a3a5f72e992d9e0a14065103afd27913
meltek13 <regnault.melvin@hotmail.fr>
2021-06-25 21:04:34 +0200

    update: heroky config

--- app/models/flatsharing.rb
+++ app/models/flatsharing.rb
@@ -6,7 +6,8 @@ class Flatsharing < ApplicationRecord
   has_many :expenses
   serialize :pending_invitation, Array
   serialize :flat_mate, Array
-
+  validates :title, presence: true
+  validates :description, presence: true
   def invitation_to_join_flat
     InvitationMailer.invitation_email(self).deliver_now
   end
